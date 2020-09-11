c767015865f0f9090df2ac2b1608743fdf0e2e8f
Markus Videfors <markus.videfors@outlook.com>
2020-09-11 16:22:31 +0200

    create test for constructing Piece

---
+++ src/pieces/tests.rs
@@ -0,0 +1,8 @@
+use super::*;
+use crate::{Color};
+
+#[test]
+fn test_new_piece() {
+    let piece = Piece::new(Color::White, Kind::King);
+    assert_eq!(piece, Piece {color: Color::White, kind: Kind::King, has_moved: false});
+}
