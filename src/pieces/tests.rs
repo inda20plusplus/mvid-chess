ce30d5b8f6b912916208623482922d22aefef926
Markus Videfors <markus.videfors@outlook.com>
2020-09-12 13:27:34 +0200

    format test

--- src/pieces/tests.rs
+++ src/pieces/tests.rs
@@ -4,5 +4,13 @@ use crate::{Color};
 #[test]
 fn test_new_piece() {
     let piece = Piece::new(Color::White, Kind::King);
-    assert_eq!(piece, Piece {color: Color::White, kind: Kind::King, has_moved: false});
+    assert_eq!(
+        piece,
+        Piece {
+            color: Color::White,
+            kind: Kind::King,
+            has_moved: false,
+        }
+    );
+}
 }
