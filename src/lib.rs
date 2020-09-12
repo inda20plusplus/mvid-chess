cf0ad4c7ca5625a6e2e7cc7c9083a262b55fa19c
Markus Videfors <markus.videfors@outlook.com>
2020-09-12 14:03:47 +0200

    derive necessary traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 mod board;
 mod pieces;
 
+#[derive(Clone, Hash, Eq, PartialEq, Debug)]
 struct Point(i8, i8);
 
+#[derive(PartialEq, Debug)]
 pub enum Color {
     White,
     Black,
 }
 
+#[derive(Clone, PartialEq, Debug)]
 pub struct Move(Point, bool);
