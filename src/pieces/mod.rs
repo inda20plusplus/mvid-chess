27c3f3a303407131b7ab6526436b611b170bf491
Markus Videfors <markus.videfors@outlook.com>
2020-09-12 13:30:14 +0200

    define and implement struct Piece

--- src/pieces/mod.rs
+++ src/pieces/mod.rs
@@ -1,7 +1,39 @@
+use crate::{Color, Move};
+
 #[cfg(test)]
 mod tests;
 
 pub mod moves;
+
+#[derive(PartialEq, Debug)]
+pub struct Piece {
+    pub color: Color,
+    pub kind: Kind,
+    pub has_moved: bool,
+}
+
+impl Piece {
+    pub fn new(color: Color, kind: Kind) -> Self {
+        Piece {
+            color,
+            kind,
+            has_moved: false,
+        }
+    }
+
+    fn get_moves(&self) -> Vec<Move> {
+        match self.kind {
+            Kind::King => moves::KING.to_vec(),
+            Kind::Queen => moves::QUEEN.to_vec(),
+            Kind::Bishop => moves::BISHOP.to_vec(),
+            Kind::Knight => moves::KNIGHT.to_vec(),
+            Kind::Rook => moves::ROOK.to_vec(),
+            Kind::Pawn => moves::PAWN.to_vec(),
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
 pub enum Kind {
     King,
     Queen,
