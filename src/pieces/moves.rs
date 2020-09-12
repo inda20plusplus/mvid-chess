ebe470cb004cb8c477d633f3de9181ccbfff8def
Markus Videfors <markus.videfors@outlook.com>
2020-09-12 13:28:51 +0200

    fix movement constants

--- src/pieces/moves.rs
+++ src/pieces/moves.rs
@@ -1,50 +1,50 @@
 use crate::{Move, Point};
 
-pub const KING: Vec<Move> = vec![
-    Move(Point(-1,1), false),
-    Move(Point(0,1), false),
-    Move(Point(1,1), false),
-    Move(Point(-1,0), false),
-    Move(Point(1,0), false),
-    Move(Point(-1,-1), false),
-    Move(Point(0,-1), false),
-    Move(Point(1,-1), false),
+pub const KING: [Move; 8] = [
+    Move(Point(-1, 1), false),
+    Move(Point(0, 1), false),
+    Move(Point(1, 1), false),
+    Move(Point(-1, 0), false),
+    Move(Point(1, 0), false),
+    Move(Point(-1, -1), false),
+    Move(Point(0, -1), false),
+    Move(Point(1, -1), false),
 ];
 
-pub const QUEEN: Vec<Move> = vec![
-    Move(Point(-1,1), true),
-    Move(Point(0,1), true),
-    Move(Point(1,1), true),
-    Move(Point(-1,0), true),
-    Move(Point(1,0), true),
-    Move(Point(-1,-1), true),
-    Move(Point(0,-1), true),
-    Move(Point(1,-1), true),
+pub const QUEEN: [Move; 8] = [
+    Move(Point(-1, 1), true),
+    Move(Point(0, 1), true),
+    Move(Point(1, 1), true),
+    Move(Point(-1, 0), true),
+    Move(Point(1, 0), true),
+    Move(Point(-1, -1), true),
+    Move(Point(0, -1), true),
+    Move(Point(1, -1), true),
 ];
 
-pub const BISHOP: Vec<Move> = vec![
-    Move(Point(-1,1), true),
-    Move(Point(1,1), true),
-    Move(Point(-1,-1), true),
-    Move(Point(1,-1), true),
+pub const BISHOP: [Move; 4] = [
+    Move(Point(-1, 1), true),
+    Move(Point(1, 1), true),
+    Move(Point(-1, -1), true),
+    Move(Point(1, -1), true),
 ];
 
-pub const ROOK: Vec<Move> = vec![
-    Move(Point(0,1), true),
-    Move(Point(-1,0), true),
-    Move(Point(1,0), true),
-    Move(Point(0,-1), true),
+pub const ROOK: [Move; 4] = [
+    Move(Point(0, 1), true),
+    Move(Point(-1, 0), true),
+    Move(Point(1, 0), true),
+    Move(Point(0, -1), true),
 ];
 
-pub const KNIGHT: Vec<Move> = vec![
-    Move(Point(-2,1), false),
-    Move(Point(-1,2), false),
-    Move(Point(1,2), false),
-    Move(Point(2,1), false),
-    Move(Point(-2,-1), false),
-    Move(Point(-1,-2), false),
-    Move(Point(1,-2), false),
-    Move(Point(2,-1), false),
+pub const KNIGHT: [Move; 8] = [
+    Move(Point(-2, 1), false),
+    Move(Point(-1, 2), false),
+    Move(Point(1, 2), false),
+    Move(Point(2, 1), false),
+    Move(Point(-2, -1), false),
+    Move(Point(-1, -2), false),
+    Move(Point(1, -2), false),
+    Move(Point(2, -1), false),
 ];
 
-pub const PAWN: Vec<Move> = vec![];
+pub const PAWN: [Move; 0] = [];
