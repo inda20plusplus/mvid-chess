3c51e60992ea4a0b7b76a5a9ea97189f4037b9e0
Markus Videfors <markus.videfors@outlook.com>
2020-09-15 00:10:47 +0200

    implement get_moves to pass all tests

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -89,4 +89,141 @@ impl Board {
 
         true
     }
+
+    pub fn get_moves(&self, source: Point) -> Vec<Point> {
+        let piece = match self.current.get(&source) {
+            Some(p) => p,
+            None => return vec![]
+        };
+
+        let mut moves: Vec<Point>;
+
+        if piece.kind == Kind::Pawn {
+            moves = self.get_moves_for_pawn(&source);
+        } else {
+            moves = self.get_moves_for_piece(&source);
+        }
+
+        if piece.kind == Kind::King {
+
+        } else {
+            if let Some(allowed_moves) = self.check_if_protecting_king(&source, &piece.color) {
+                moves.retain(|point| allowed_moves.contains(&point));
+            }
+        };
+
+        moves
+    }
+
+    fn check_if_protecting_king(&self, source: &Point, color: &Color) -> Option<Vec<Point>> {
+        let king = self.king_pos.get(&color).unwrap();
+
+        if let Some(direction) = king.relative_direction(&source) {
+            let mut current_position = source.add(&direction);
+            let mut passed_points: Vec<Point> = vec![];
+            loop {
+                passed_points.push(current_position.clone());
+                if !self.current.contains_key(&current_position) {
+                    if !(1..=8).contains(&current_position.0) || !(1..=8).contains(&current_position.1) {
+                        return None
+                    } else {
+                        current_position = current_position.add(&direction);
+                        continue
+                    };
+                } else {
+                    let target_piece = self.current.get(&current_position).unwrap();
+                    if &target_piece.color == color {
+                        return None
+                    } else {
+                        if direction.0 == 0 || direction.1 == 0 {
+                            if [Kind::Queen, Kind::Rook].contains(&target_piece.kind) {
+                                return Some(passed_points)
+                            } else {
+                                return None
+                            };
+                        } else {
+                            if [Kind::Queen, Kind::Bishop].contains(&target_piece.kind) {
+                                return Some(passed_points)
+                            } else {
+                                return None
+                            };
+                        };
+                    };
+                };
+            };
+        } else {
+            return None
+        };
+    }
+
+    fn get_moves_for_pawn(&self, source: &Point) -> Vec<Point> {
+        let piece = self.current.get(&source).unwrap();
+        if piece.kind != Kind::Pawn {
+            panic!("Piece is not of kind pawn");
+        };
+
+        let direction = match piece.color {
+            Color::White => Point(0, 1),
+            Color::Black => Point(0, -1)
+        };
+
+        let mut moves: Vec<Point> = vec![];
+
+        if !self.current.contains_key(&source.add(&direction)) {
+            moves.push(source.add(&direction));
+        };
+
+        if let Some(target) = self.current.get(&source.add(&direction.add(&Point(1, 0)))) {
+            if target.color != piece.color {
+                moves.push(source.add(&direction.add(&Point(1, 0))));
+            };
+        };
+
+        if let Some(target) = self.current.get(&source.add(&direction.add(&Point(-1, 0)))) {
+            if target.color != piece.color {
+                moves.push(source.add(&direction.add(&Point(-1, 0))));
+            };
+        };
+
+        if !piece.has_moved && !self.current.contains_key(&source.add(&direction).add(&direction)) {
+            moves.push(source.add(&direction).add(&direction));
+        };
+
+        moves
+    }
+
+    fn get_moves_for_piece(&self, source: &Point) -> Vec<Point> {
+        let piece = self.current.get(&source).unwrap();
+        if piece.kind == Kind::Pawn {
+            panic!("Piece cannot be of type Pawn");
+        };
+
+        let mut moves: Vec<Point> = vec![];
+        
+        for mv in piece.get_moves() {
+            let mut current_point = source.add(&mv.0);
+            loop {
+                if !self.current.contains_key(&current_point) {
+                    if !(1..=8).contains(&current_point.0) || !(1..=8).contains(&current_point.1) {
+                        break
+                    }
+                    moves.push(current_point.clone());
+                } else {
+                    let target_piece = self.current.get(&current_point).unwrap();
+                    if target_piece.color != piece.color {
+                        moves.push(current_point);
+                    }
+                    break
+                }
+
+                if mv.1 {
+                    current_point = current_point.add(&mv.0);
+                } else {
+                    break
+                };
+            }
+        }
+
+        moves
+    }
 }
