98f6eb7afe57bb58143ded9c4ae01d33c16b71f2
Markus Videfors <markus.videfors@outlook.com>
2020-09-19 17:56:37 +0200

    create basic skeleton of pub methods

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -4,14 +4,46 @@ mod tests;
 use crate::board::Board;
 use crate::*;
 
-struct Game {
-    board: Board,
-    current_turn: Color,
+pub enum EndResult {
+    Win(Color),
+    Tie,
+}
+
+pub enum TurnResult {
+    Moved,
+    Checked,
+    GameEnd(EndResult),
+    Failed
+}
+
+pub struct Game {
+    pub board: Board,
+    pub turn: Color,
+    pub checked: Option<Vec<Point>>,
 }
 
 impl Game {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Game {
+            board: Board::new(),
+            turn: Color::White,
+            checked: None,
         }
+    }
+
+    pub fn turn(&self, source: Point, target: Point) -> TurnResult {
+        // Check if move is valid, return TurnResult::Failed
+        // Make move
+        // Set turn to opposing player
+        // set self.checked to contain points of any pieces checking the current color
+        // Check for game over
+        // May be reordered and changed ^
+        TurnResult::Moved
+    }
+
+    pub fn get_moves(&self, source: Point) -> Option<Vec<Point>> {
+        // return Some(vector of points that source can move to)
+        // returns None if there is no piece there
+        None
     }
 }
