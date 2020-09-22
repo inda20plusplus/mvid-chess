173b1ef4cc10e2e9f0e5fb2ccd065c994bafe78c
Markus Videfors <markus.videfors@outlook.com>
2020-09-22 16:43:04 +0200

    implement game logic for turn

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,5 +1,5 @@
-use crate::*;
 use crate::board::Board;
+use crate::*;
 
 #[cfg(test)]
 mod tests;
@@ -32,18 +32,56 @@ impl Game {
     }
 
     pub fn turn(&mut self, source: Point, target: Point) -> TurnResult {
-        // Check if move is valid, return TurnResult::Failed
-        // Make move
-        // Set turn to opposing player
-        // set self.checked to contain points of any pieces checking the current color
-        // Check for game over
-        // May be reordered and changed ^
-        TurnResult::Moved
+        if let Some(piece) = self.board.current.get(&source) {
+            if piece.color != self.color {
+                println!("1");
+                return TurnResult::Failed
+            };
+        };
+
+        if self.board.get_allowed_moves(&source).contains(&target) {
+            if !self.board.move_piece(source, target) {
+                println!("2");
+                return TurnResult::Failed;
+            }
+        } else {
+            println!("3");
+            return TurnResult::Failed;
+        }
+        
+        let opponent_can_move = self.color_can_move(&self.color.inverse());
+
+        let opponent_is_checked = match self.board.detect_check(&self.color.inverse()) {
+            Some(_) => true,
+            None => false,
+        };
+
+        if opponent_is_checked && opponent_can_move {
+            self.color = self.color.inverse();
+            return TurnResult::Checked;
+        } else if opponent_is_checked && !opponent_can_move {
+            return TurnResult::GameEnd(EndResult::Win(self.color.clone()));
+        } else if !opponent_is_checked && !opponent_can_move {
+            return TurnResult::GameEnd(EndResult::Tie);
+        } else {
+            self.color = self.color.inverse();
+            return TurnResult::Moved;
+        }
     }
 
-    pub fn get_moves(&self, source: Point) -> Option<Vec<Point>> {
-        // return Some(vector of points that source can move to)
-        // returns None if there is no piece there
-        None
+    fn color_can_move(&mut self, color: &Color) -> bool {
+        for x in self.board.width.clone() {
+            for y in self.board.height.clone() {
+                let point = Point(x, y);
+                if let Some(piece) = self.board.current.get(&point) {
+                    if &piece.color == color {
+                        if self.board.get_allowed_moves(&point).len() > 0 {
+                            return true
+                        }
+                    }
+                }
+            }
+        }
+        false 
     }
 }
